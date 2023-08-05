496cad14b831f8f0e463a276d9486d87db7ad8fe
Ani Abaishvili <110342939+aniabaishvili@users.noreply.github.com>
2023-08-05 17:00:59 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = document.querySelector('.slider');
+    const slides = slider.querySelectorAll('div');
+    const prevBtn = document.querySelector('.left');
+    const nextBtn = document.querySelector('.right');
+    let currentIndex = 0;
+  
+    nextBtn.addEventListener('click', () => {
+      if (currentIndex < slides.length - 1) {
+        currentIndex++;
+        updateSlider();
+        slider.style.transition = 'transform 0.4s ease-in-out';
+      }
+    });
+  
+    prevBtn.addEventListener('click', () => {
+      if (currentIndex > 0) {
+        currentIndex--;
+        updateSlider();
+        slider.style.transition = 'transform 0.4s ease-in-out';
+      }
+    });
+  
+    function updateSlider() {
+      const offset = -currentIndex * slides[0].clientWidth;
+      slider.style.transform = `translateX(${offset}px)`;
+    }
+  });
+  
